774a422cf6d6bf6b6c23c6f366bde2caa01b4e37
Farshad Nematdoust <farshad@nematdoust.com>
2021-10-12 22:19:46 +0200

    Update time.go, using Ceil instead of Round, so it will be more predictable.

--- helpers/time.go
+++ helpers/time.go
@@ -8,7 +8,7 @@ import (
 // IsDeletable check if the tweet is within the offset user provided. (offset is in days)
 func IsDeletable(createdAt time.Time, offset float64) bool {
 	now := time.Now()
-	t := math.Round(now.Sub(createdAt).Hours() / 24)
+	t := math.Ceil(now.Sub(createdAt).Hours() / 24)
 	if t > offset {
 		return true
 	}
