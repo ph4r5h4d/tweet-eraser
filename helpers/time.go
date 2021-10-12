7c72b6f35a4e13f6fa393e5125792d2576cceda1
Farshad Nematdoust <farshad@nematdoust.com>
2021-10-12 22:07:11 +0200

    Add a small helper to check whether tweet time is within the limit user requested or not.

---
+++ helpers/time.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"math"
+	"time"
+)
+
+// IsDeletable check if the tweet is within the offset user provided. (offset is in days)
+func IsDeletable(createdAt time.Time, offset float64) bool {
+	now := time.Now()
+	t := math.Round(now.Sub(createdAt).Hours() / 24)
+	if t > offset {
+		return true
+	}
+	return false
+}
